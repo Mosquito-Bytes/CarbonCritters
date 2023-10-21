15c6e431ee27108d09c35164149373eec3fc4e2f
Plamen Totev <plamen.iv.totev@gmail.com>
2023-10-21 18:33:29 +0300

    Update clients on leaderboard change

--- backend/src/main/java/mosquitobytes/carboncritters/service/ProductService.java
+++ backend/src/main/java/mosquitobytes/carboncritters/service/ProductService.java
@@ -1,9 +1,12 @@
 package mosquitobytes.carboncritters.service;
 
 import mosquitobytes.carboncritters.connector.ProductDatabaseConnector;
+import mosquitobytes.carboncritters.handler.CustomWebSocketHandler;
 import mosquitobytes.carboncritters.model.ConsumedProduct;
 import mosquitobytes.carboncritters.repository.ConsumedProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class ProductService {
@@ -14,10 +17,13 @@ public class ProductService {
 
     private final ProductDatabaseConnector productDatabase;
 
-    public ProductService(ConsumedProductRepository consumedProducts, ProfileService profileService, ProductDatabaseConnector productDatabase) {
+    private final CustomWebSocketHandler webSocketHandler;
+
+    public ProductService(ConsumedProductRepository consumedProducts, ProfileService profileService, ProductDatabaseConnector productDatabase, CustomWebSocketHandler webSocketHandler) {
         this.consumedProducts = consumedProducts;
         this.profileService = profileService;
         this.productDatabase = productDatabase;
+        this.webSocketHandler = webSocketHandler;
     }
 
     public void addProduct(Long consumerId, String productCode) {
@@ -27,6 +33,12 @@ public class ProductService {
         consumedProducts.save(consumedProduct);
 
         profileService.updateScore(consumerId, consumedProduct.product().footprint());
+
+        try {
+            webSocketHandler.sendLeaderBoardToAllActiveSessions();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
